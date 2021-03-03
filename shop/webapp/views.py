705b0f15f6a5220c4749b4d640286e9d3337c71a
Tim <timson995@gmail.com>
2021-03-03 12:57:07 +0600

    Templates:Fixed Category. Now Parent Category show childrens items

--- shop/webapp/views.py
+++ shop/webapp/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render, get_list_or_404, get_object_or_404
 from django.views.generic import ListView, DetailView
 from webapp.models import Category, Item
 from django.utils import timezone
-from mptt.exceptions import InvalidMove
 from django.urls import reverse
 from django.db.models import Q
 import mptt
@@ -24,16 +23,20 @@ class IndexView(ListView):
                     'category': Category._default_manager.all()}
         return queryset
 
+
 class CategoryListView(ListView):
     model = Item
     context_object_name = 'data'
     template_name = 'webapp/index_view.html'
 
     def get_queryset(self):
-        queryset = {'items': Item.objects.filter(category_id=self.kwargs.get('pk')),
+        items = Item.objects.filter(Q(category__id=self.kwargs.get('pk')) | Q(category__parent_id=self.kwargs.get('pk'))) 
+        queryset={'items': items,
                     'category': Category.objects.all()}
         return queryset
 
-        # queryset = {'items': Item.objects.filter(category_id=self.kwargs.get('pk')),
-        #             'category': Category.objects.all()}
-        # return queryset
+
+
+    # queryset = {'items': Item.objects.filter(category_id=self.kwargs.get('pk')),
+    #             'category': Category.objects.all()}
+    # return queryset
